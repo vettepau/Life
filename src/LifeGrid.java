10af09960ebc3f6b0f5d3353c8a696f954602c1d
jewbank <jewbank@conroeisd.net>
2020-02-13 12:48:18 -0600

    Class Updates

--- src/LifeGrid.java
+++ src/LifeGrid.java
@@ -3,14 +3,24 @@ public class LifeGrid
     private boolean[][] grid;
 
     LifeGrid(int w, int h) {
-        grid = new boolean[h][w];
+        grid = new boolean[h+2][w+2];
     }
 
     @Override
     public String toString() {
         String out = "";
+        boolean a = true;
         for(boolean[] row: grid){
+            if(a){
+                a = false;
+                continue;
+            }
+            boolean b = true;
             for(boolean cell: row){
+                if(b){
+                    b = false;
+                    continue;
+                }
                 out += "[" + (cell?"X":" ") + "]";
             }
             out += "\n";
@@ -19,18 +29,49 @@ public class LifeGrid
     }
 
     public int rows(){
-        return grid.length;
+        return grid.length - 2;
     }
 
     public int cols(){
-        return grid[0].length;
+        return grid[0].length - 2;
     }
 
     public void place(int row, int col) {
-        grid[row][col] = true;
+        grid[row+1][col+1] = true;
     }
 
+
     public void next() {
-        System.out.println("LifeGrid.next");
+        boolean[][] tempGrid = new boolean[rows()+2][cols()+2];
+
+        for(int row = 1; row <= rows(); row++){
+            for(int col = 1; col <= cols(); col++){
+                // 2 cases
+                // count neighbors
+                int neighbors = numNeighbors(row,col);
+
+                if(grid[row][col]){
+                    // 1 - alive
+                    tempGrid[row][col] = (neighbors == 2 || neighbors == 3);
+                } else {
+                    // 2 - dead
+                    tempGrid[row][col] = (neighbors == 3);
+                }
+            }
+        }
+        grid = tempGrid;
+    }
+
+    private int numNeighbors(int row, int col){
+        int count = 0;
+        count += grid[row - 1][col - 1]?1:0;
+        count += grid[row - 1][col]?1:0;
+        count += grid[row - 1][col + 1]?1:0;
+        count += grid[row][col - 1]?1:0;
+        count += grid[row][col + 1]?1:0;
+        count += grid[row + 1][col - 1]?1:0;
+        count += grid[row + 1][col]?1:0;
+        count += grid[row + 1][col + 1]?1:0;
+        return count;
     }
 }
--- src/TextRunner.java
+++ src/TextRunner.java
@@ -1,11 +1,17 @@
 public class TextRunner {
 
     public static void main(String[] args) {
-        LifeGrid lg = new LifeGrid(15,15);
+        LifeGrid lg = new LifeGrid(15,10);
 
-        for(int i = 0; i < 20; i++){
-            lg.place((int)(Math.random() * lg.rows()), (int)(Math.random() * lg.cols()));
-        }
+        lg.place(2,1);
+        lg.place(2,2);
+        lg.place(2,3);
+
+
+        lg.place(0,5);
+        lg.place(0,6);
+        lg.place(0,7);
+        lg.place(0,8);
 
         System.out.println(lg);
         lg.next();
